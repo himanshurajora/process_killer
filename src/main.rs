898d6086bb494e0594c0dd6bde52c3669e40c2b6
Himanshu Jangid <iamahalofan@gmail.com>
2023-03-26 00:16:35 +0530

    searching feature

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use sysinfo::{Pid, ProcessExt, System, SystemExt};
 use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Layout},
-    style::{Color, Modifier, Style},
+    style::{Color, Style},
     text::{Span, Spans, Text},
     widgets::{Block, Borders, Cell, Paragraph, Row, Table, TableState},
     Frame, Terminal,
@@ -21,10 +21,17 @@ enum SortByNameOptions {
     NONE,
 }
 
+enum InputMode {
+    NORMAL,
+    EDITING,
+}
+
 struct App {
     state: TableState,
     processes: Vec<(Pid, String)>,
     sort_by_name_option: SortByNameOptions,
+    search_input: String,
+    input_mode: InputMode,
 }
 
 impl App {
@@ -40,6 +47,8 @@ impl App {
             processes: processes,
             state: TableState::default(),
             sort_by_name_option: SortByNameOptions::NONE,
+            search_input: String::new(),
+            input_mode: InputMode::NORMAL,
         }
     }
 
@@ -96,12 +105,40 @@ impl App {
         let process = self.processes[self.state.selected().unwrap()].clone();
         let s = System::new_all();
         s.process(process.0).unwrap().kill();
+        self.refetch_process()
+    }
+
+    pub fn refetch_process(&mut self) {
+        let s = System::new_all();
 
         self.processes.clear();
         for process in s.processes() {
             self.processes
                 .push((*process.0, process.1.name().to_string()));
         }
+    }
+
+    pub fn enter_input_mode(&mut self) {
+        self.input_mode = InputMode::EDITING;
+    }
+
+    pub fn exit_input_mode(&mut self) {
+        self.input_mode = InputMode::NORMAL;
+    }
+
+    pub fn search(&mut self) {
+        self.refetch_process();
+        if self.search_input == "" {
+            return;
+        }
+        let mut filtered_process: Vec<(Pid, String)> = vec![];
+        self.processes.iter().for_each(|(pid, name)| {
+            if self.search_input.contains(name) || name.contains(&self.search_input.to_string()) {
+                filtered_process.push((*pid, name.to_string()));
+            }
+        });
+
+        self.processes = filtered_process;
     }
 }
 
@@ -143,15 +180,28 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
         terminal.draw(|f| ui(f, app))?;
 
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Down => app.next(),
-                KeyCode::Up => app.prev(),
-                KeyCode::Enter => app.kill(),
-                KeyCode::Char('q') => return Ok(()),
-                KeyCode::Char('n') => app.switch_sort(),
-                KeyCode::Char('j') => app.next(),
-                KeyCode::Char('k') => app.prev(),
-                _ => {}
+            match app.input_mode {
+                InputMode::NORMAL => match key.code {
+                    KeyCode::Down => app.next(),
+                    KeyCode::Up => app.prev(),
+                    KeyCode::Enter => app.kill(),
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Char('n') => app.switch_sort(),
+                    KeyCode::Char('j') => app.next(),
+                    KeyCode::Char('k') => app.prev(),
+                    KeyCode::Char('i') => app.enter_input_mode(),
+                    KeyCode::Char('r') => app.refetch_process(),
+                    _ => {}
+                },
+                InputMode::EDITING => match key.code {
+                    KeyCode::Esc => app.exit_input_mode(),
+                    KeyCode::Backspace => {
+                        app.search_input.pop();
+                    }
+                    KeyCode::Enter => app.search(),
+                    KeyCode::Char(c) => app.search_input.push(c),
+                    _ => {}
+                },
             }
         }
     }
@@ -159,6 +209,10 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let size = f.size();
+
+    let input_block = Block::default()
+        .title("Search process by name")
+        .borders(Borders::ALL);
 
     let main_block = Block::default()
         .borders(Borders::ALL)
@@ -190,31 +244,46 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .highlight_style(selected_style)
         .highlight_symbol(">> ")
         .widths(&[
-            Constraint::Percentage(50),
-            Constraint::Length(30),
-            Constraint::Min(10),
+            Constraint::Percentage(10),
+            Constraint::Length(20),
+            Constraint::Min(70),
         ]);
 
     let chunks = Layout::default()
         .direction(tui::layout::Direction::Vertical)
-        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)])
+        .constraints([
+            Constraint::Percentage(10),
+            Constraint::Percentage(80),
+            Constraint::Percentage(10),
+        ])
         .split(size);
 
     let instructions = vec![
-        Span::from("1. Press"),
-        Span::styled(" Enter ", Style::default().add_modifier(Modifier::BOLD)),
-        Span::from("to kill a process, 2. Press"),
-        Span::styled(" N ", Style::default().add_modifier(Modifier::BOLD)),
-        Span::from("to toggle sort by Name, 3. Press"),
-        Span::styled(" Q ", Style::default().add_modifier(Modifier::BOLD)),
-        Span::from("to quit"),
-        Span::from(", Press J and K or Up and Down array for navigating through the list"),
+        Span::from("'Enter to Kill', 'N for toggle sorting', 'Q to quit', 'I to input mode', 'Esc to exit input mode', 'Enter to search'"),
     ];
 
     let sort_name_text = Text::from(Spans::from(instructions));
     let paragraph = Paragraph::new(sort_name_text).block(instruction_block);
 
-    f.render_stateful_widget(table, chunks[0], &mut app.state);
+    let mut char_spans = vec![];
+    app.search_input
+        .chars()
+        .into_iter()
+        .for_each(|c| char_spans.push(Span::from(c.to_string())));
+
+    let input_text = Text::from(Spans::from(char_spans));
+    let width = input_text.width() as u16;
+
+    let input_paragraph = match &app.input_mode {
+        InputMode::NORMAL => Paragraph::new(app.search_input.as_ref()).block(input_block),
+        InputMode::EDITING => Paragraph::new(input_text)
+            .style(Style::default().fg(Color::Yellow))
+            .block(input_block),
+    };
+
+    f.set_cursor(chunks[0].x + width + 1, chunks[0].y + 1);
+    f.render_widget(input_paragraph, chunks[0]);
+    f.render_stateful_widget(table, chunks[1], &mut app.state);
     // f.render_widget(instruction_block, chunks[1]);
-    f.render_widget(paragraph, chunks[1]);
+    f.render_widget(paragraph, chunks[2]);
 }
